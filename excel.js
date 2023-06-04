523c9b94cdef978fda60cb8b8e720f74588930a6
linxl <3382272560@qq.com>
2023-06-04 15:29:24 +0800

    update

--- excel.js
+++ excel.js
@@ -1,13 +1,14 @@
 /**
- * Copyright (c) 2014-2019 Guyon Roche
+ * Copyright (c) 2023-now zurmokeeper
  * LICENCE: MIT - please refer to LICENSE file included with this module
- * or https://github.com/exceljs/exceljs/blob/master/LICENSE
+ * or https://github.com/zurmokeeper/excelize/blob/master/LICENSE
  */
 
 if (parseInt(process.versions.node.split('.')[0], 10) < 10) {
   throw new Error(
-    'For node versions older than 10, please use the ES5 Import: https://github.com/exceljs/exceljs#es5-imports'
+    'For node versions older than 10, please use the ES5 Import: https://github.com/zurmokeeper/excelize#es5-imports'
   );
 }
 
+// eslint-disable-next-line import/extensions
 module.exports = require('./lib/exceljs.nodejs.js');
